12565e6f1f10e75f58094c50639a2acb712abe5d
Djalma Manfrin <djalma.manfrin@gmail.com>
2018-09-16 23:47:59 -0300

    notetions

--- src/main/java/com/spark/App.java
+++ src/main/java/com/spark/App.java
@@ -1,15 +1,12 @@
 package com.spark;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
 
     public static void main( String[] args )
     {
         new Config().setConfig();
         new Routes().setRoutes();
+        // carregar csv aqui
     }
 }
