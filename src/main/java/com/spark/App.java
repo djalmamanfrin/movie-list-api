26c922b4c5da5a5ae7c97ac8dd5cfc180625029f
Djalma Manfrin <djalma.manfrin@rentcars.com>
2018-09-19 17:51:34 -0300

    application configs

--- src/main/java/com/spark/App.java
+++ src/main/java/com/spark/App.java
@@ -1,14 +1,13 @@
 package com.spark;
 
-import com.spark.configs.ApplicationServerConfiguration;
+import com.spark.http.ApplicationServer;
+import com.spark.utils.csv.CsvFileReader;
 
 public class App
 {
-
     public static void main( String[] args )
     {
-        new ApplicationServerConfiguration().setConfig();
-        new Routes().setRoutes();
-        // carregar csv aqui
+        new ApplicationServer();
+        new CsvFileReader().getMovies();
     }
 }
